74dad9c216b1079dccf5fc6c3f797ff3328047cd
Erika Smith <erikaannesmith@gmail.com>
2018-04-01 09:43:46 -0700

    full TDD with CRUD functionality for styles

--- app/controllers/user/styles_controller.rb
+++ app/controllers/user/styles_controller.rb
@@ -1,7 +1,46 @@
 class User::StylesController < ApplicationController
 
   def show
+    @style = Style.find_by(id: params[:id], client_id: params[:client_id])
+  end
 
+  def new
+    @style = Style.new
+  end
+
+  def create
+    client = Client.find(params[:client_id])
+    @style = client.styles.create(style_params)
+    if @style.save!
+      flash[:success] = "#{@style.name} has been created!"
+      redirect_to user_client_style_path(client, @style)
+    else
+      redirect_to user_client_path(@style.client)
+      flash[:danger] = "#{@style.name} could not be created, please try again!"
+    end
+  end
+
+  def edit
+    client = Client.find(params[:client_id])
+    @style = client.styles.find(params[:id])
+  end
+
+  def update
+    @style = Style.find(params[:id])
+    @style.update(style_params)
+    if @style.save!
+      redirect_to user_client_style_path(@style.client, @style)
+      flash[:success] = "#{@style.name} has been updated!"
+    else
+      redirect_to user_client_path(@style.client)
+      flash[:danger] = "#{@style.name} could not be updated, please try again!"
+    end
+  end
+
+  private
+
+  def style_params
+    params.require(:style).permit(:name, :amount, :cost, :seasons)
   end
 
 end
--- spec/features/user_interacts_with_styles_spec.rb
+++ spec/features/user_interacts_with_styles_spec.rb
@@ -65,10 +65,71 @@ describe "User interacts with styles" do
     click_on client_1.name
     click_on style_1.name
 
-    expect(current_path).to eq(user_client_style_path(style_1))
+    expect(current_path).to eq(user_client_style_path(client_1, style_1))
     expect(page).to have_content(style_1.name)
     expect(page).to have_content(style_1.amount)
     expect(page).to have_content(style_1.cost)
     expect(page).to have_content(style_1.season)
+  end
+
+  it "They can create a new style" do
+    stub_omniauth
+    visit root_path
+    click_on "Sign in with Google"
+    user = User.first
+    client_1 = Client.create(name: "Stone Cold Fox",
+                             email: "stone@stone.com",
+                             user: user)
+    
+    expect(client_1.styles.count).to eq(0)
+
+    visit root_path
+    click_on client_1.name
+    click_on "Create Style"
+
+    expect(current_path).to eq(new_user_client_style_path(client_1))
+
+    fill_in "style[name]", with: "Elaina Top"
+    fill_in "style[amount]", with: "60"
+    fill_in "style[cost]", with: "80"
+    fill_in "style[season]", with: "SS17"
+
+    click_on "Create Style"
+
+    style = Style.last
+    expect(current_path).to eq(user_client_style_path(client_1, style))
+    expect(page).to have_content("#{style.name} has been created!")
+    expect(page).to have_content(style.amount)
+    expect(page).to have_content(style.cost)
+    expect(page).to have_content(style.season)
+    expect(client_1.styles.count).to eq(1)
+  end
+
+  it "They can edit an existing style" do
+    stub_omniauth
+    visit root_path
+    click_on "Sign in with Google"
+    user = User.first
+    client_1 = Client.create(name: "Stone Cold Fox",
+                             email: "stone@stone.com",
+                             user: user)
+    style_1 = Style.create(name: "Belle Top",
+                           amount: 40,
+                           cost: 10.0,
+                           season: "SS16",
+                           client: client_1)
+    
+    visit user_client_style_path(client_1, style_1)
+
+    expect(page).to have_content("Belle Top")
+
+    click_on "Edit Style"
+    fill_in "style[name]", with: "Erika Top"
+    click_on "Update Style"
+
+    expect(current_path).to eq(user_client_style_path(client_1, style_1))
+    expect(page).to have_content("Erika Top has been updated!")
+    expect(page).not_to have_content("Belle")
+    expect(client_1.styles.count).to eq(1)
   end
 end
