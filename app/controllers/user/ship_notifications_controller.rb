254f35e2719a2dd54094a39aed4f2a11fc9a855b
Erika Smith <erikaannesmith@gmail.com>
2018-04-23 14:57:37 -0700

    selector for emails client name

--- app/controllers/user/ship_notifications_controller.rb
+++ app/controllers/user/ship_notifications_controller.rb
@@ -18,5 +18,6 @@ class User::ShipNotificationsController < ApplicationController
   def send_shipping_notification(recipient, tracking_number, shipping_service, additional_notes, sender_email, sender_name, shipping_reciept, shipping_receipt_name)
     UserMailer.welcome_email(recipient, tracking_number, shipping_service, additional_notes, sender_email, sender_name, shipping_reciept, shipping_receipt_name).deliver    
     redirect_to user_ship_notifications_path(current_user)
+    flash[:notice] = "Shipping notification has been sent!"
   end
 end
