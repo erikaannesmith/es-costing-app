4c72091527a0958d36b9a33da86f951b163e81c4
Erika Smith <erikaannesmith@gmail.com>
2018-04-25 10:28:52 -0700

    abbreviated style names and styled style show name

--- app/models/style.rb
+++ app/models/style.rb
@@ -7,4 +7,11 @@ class Style < ApplicationRecord
                     :path => ':id/:style/:filename'
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def abbreviated_name
+    letters = self.name.chars
+    letters.length > 20 ? abb_name = letters[0..19].push('...') : abb_name = letters
+    abb_name.join('')
+  end
+
 end
